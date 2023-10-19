8db3ba72ea67c95e5c45aef6f330a66335657e01
vatasuk <vatasuk111@gmail.com>
2023-10-19 19:25:31 +0300

    gfа

--- src/main/java/com/example/lab3/MainApp.java
+++ src/main/java/com/example/lab3/MainApp.java
@@ -6,6 +6,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
@@ -23,6 +24,7 @@ public class MainApp extends Application {
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("AddressApp");
+        this.primaryStage.getIcons().add(new Image("file:resources/images/ma.png"));
         initRootLayout();
         showCityOverview();
     }
